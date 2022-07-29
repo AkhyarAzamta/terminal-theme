472ca4a2d9b67e0bdc6cbb36bb23dcc08d2ff4e3
AkhyarAzamta <45706780+AkhyarAzamta@users.noreply.github.com>
2022-07-29 14:40:51 +0700

    Add files via upload

---
+++ screen.sh
@@ -0,0 +1,17 @@
+apt install ruby
+gem install lolcat
+apt install screenfetch
+
+echo
+echo "COPY DIBAWAH INI TRUS ENTER TRUS PASTE DI PALING ATAS\n"
+sleep 4s
+echo "clear"
+echo "date | lolcat"
+echo "echo ============================== | lolcat"
+echo "screenfetch -A Linux"
+echo "cal"
+echo "echo"
+echo "prompt_context(){prompt_segment 'red' '' '⚡akhyar.azamta⚡'}"
+echo
+read a
+nano ~./zshrc
